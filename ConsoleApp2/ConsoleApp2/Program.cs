77cebefc13225a0c9677909a856bf0f12412f3ba
Tamerlan Kh. Alimardanli <tamerlankha@code.local>
2022-05-12 19:02:27 +0400

    added Home task 12.05.2022

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.Write("Yashinizi daxil edin ");
+			string ageStr = Console.ReadLine();
+			bool isParsable = int.TryParse(ageStr, out int age);
+			int[] ages = { 17, 19, 22, 25, 27, 32 };
+			bool flag = false;
+			foreach (int el in ages)
+			{
+				if (!isParsable)
+				{
+					Console.WriteLine("Yashinizi duzgun daxil edin");
+					break;
+				}
+				else 
+				{
+					if(age == el)
+                    {
+						flag = true;
+						
+						break;
+						
+                      
+					}
+					
+
+
+
+
+				}
+
+				
+			}Console.WriteLine(flag);
+			
+
+		}
+
+
+	}
+}
