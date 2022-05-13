7b2ba6f3c252c50b455fa6b456ce8122fbea1a46
Tamerlan Kh. Alimardanli <tamerlankha@code.local>
2022-05-13 18:49:27 +0400

    Added hometask2.13.05.2022

---
+++ ConsoleApphw13.05.2022/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] grade = { 10, 20, 20 };
+			int Avar = CalcAvg(grade);
+			if (Avar >= 60)
+			{
+				Console.WriteLine("Mezun oldunuz");
+			}
+			else
+			{
+				Console.WriteLine("Mezun ola bilmediniz");
+			}
+		}
+
+		static int CalcAvg(int[] arr)
+		{
+			int count = 0;
+			int sum = 0;
+			foreach (int el in arr)
+			{
+				sum += el;
+				count++;
+			}
+			int res = sum / count;
+			return res;
+		}
+    }
+}
