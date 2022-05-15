bce6c543895e31e5a9dfd14a20f01c99850c4691
Tamerlan9129 <104448579+Tamerlan9129@users.noreply.github.com>
2022-05-15 15:45:24 +0400

    Add files via upload
    homwk 14.05.2022

---
+++ Hw14.05.2022/Hw14.05.2022/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Hw14._05._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int [] arr = {5, 6, 6, };
+            //Console.WriteLine(arraySumEvenOdd(arr));
+            PrintPyramid(5);
+        }
+        static void PrintPyramid(int en){
+            
+            
+            
+                for (int i = 0; i < en; i++)
+                {
+                for (int j = 0; j <=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+                
+
+            
+                
+            
+            
+        }
+        #region task 1
+        //static bool arraySumEvenOdd(int[] arr)
+        //{
+        //    int sum = 0;
+        //    bool flag = false;
+        //    foreach (int el in arr)
+        //    {
+        //        sum += el;
+        //    }
+        //    if (sum % 2 == 0)
+        //    {
+        //        flag = true;
+        //        return flag;
+        //    }
+        //    else
+        //    {
+                
+        //        return flag;
+        //    }
+            
+        //}
+        #endregion
+    }
+
+}
