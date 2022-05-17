887035afa5d66cbd67f2dca1bb2348c1092ac3b3
unknown <tamerlankha@code.edu.az>
2022-05-17 18:50:54 +0400

    Homew 17.05.2022

---
+++ homework17.05.2022/homework17.05.2022/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homework17._05._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nurlan Emil Tural Yusif";
+            
+            Console.WriteLine(ReverseName(name));
+        }
+
+        static string ReverseName(string name)
+        {
+            string revName = "";
+            string[] nameArr = name.Split(' ');
+
+            foreach(var item in nameArr)
+            {
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+
+                    revName += item[i];
+                }
+                revName += " ";
+            }
+            return revName;
+
+        }
+    }
+}
