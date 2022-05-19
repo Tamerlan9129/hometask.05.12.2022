72a0b7881df2efb667d76561c4c3f1a9e8b79c74
unknown <tamerlankha@code.edu.az>
2022-05-19 18:50:07 +0400

    Homew 19.05.2022

---
+++ hw19.05.2022/ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr1 = { 2,8,12,33};
+            int[] arr2 = { 12,33 };
+            Console.WriteLine(IsArrayContains(arr1,arr2));
+
+        }
+        static bool IsArrayContains(int [] arr, int[] arr2) {
+
+            string convarr = string.Join(",", arr);
+            string convarr2 = string.Join(",",arr2);
+            if (convarr.Contains(convarr2)) {
+
+                    return true;
+                
+               
+            }
+            return false;
+        }
+    }
+}
