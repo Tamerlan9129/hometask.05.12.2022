b1aa081726608154c72dace3aefb2c31c3af284d
unknown <tamerlankha@code.edu.az>
2022-05-24 18:42:40 +0400

    Home24.05.2022

---
+++ Homewk24.05.2022/Homewk24.05.2022/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homewk24._05._2022
+{
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+
+        //public Person(string name, string surname) 
+        //{
+        //    //Name = name;
+        //    //Surname = surname;
+
+        //}
+        public Person(string name,string surname,int age)//:this(name,surname)
+        {
+        Name = name;
+        Surname = surname;
+        Age = age;
+            if (Age > 18)
+            {
+                IsAdult = true;
+            }
+            else {
+                IsAdult = false;
+            }
+         }
+    }
+    
+}
---
+++ Homewk24.05.2022/Homewk24.05.2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homewk24._05._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person man1 = new Person("Tamerlan", "Alimardanli", 30);
+            Console.WriteLine(man1.IsAdult);
+        }
+    }
+}
